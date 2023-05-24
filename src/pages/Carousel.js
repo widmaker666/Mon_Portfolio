5bf2a6e8fffb8f1d2fe63fdd39e832953d99d2d0
widmaker666 <alexandre.hontcharouk69@gmail.com>
2023-05-24 19:38:34 +0200

    modification effacé et remis à jour le carousel marche simplement

--- src/pages/Carousel.js
+++ src/pages/Carousel.js
@@ -46,29 +46,11 @@ const Carousel = () => {
     switch (count) {
       case 1:
         document.getElementById("oldskool").style.zIndex = 1;
-        document.getElementById("oldskool1").style.zIndex = 1;
-        document.getElementById("oldskool2").style.zIndex = 1;
-        document.getElementById("oldskool3").style.zIndex = 1;
-        document.getElementById("oldskool4").style.zIndex = 1;
-        document.getElementById("oldskool5").style.zIndex = 1;
-        document.getElementById("oldskool6").style.zIndex = 1;
         document.querySelector(".switch-left").style.display = "block";
         break;
       case 2:
         document.getElementById("oldskool").style.zIndex = 0;
-        document.getElementById("oldskool1").style.zIndex = 0;
-        document.getElementById("oldskool2").style.zIndex = 0;
-        document.getElementById("oldskool3").style.zIndex = 0;
-        document.getElementById("oldskool4").style.zIndex = 0;
-        document.getElementById("oldskool5").style.zIndex = 0;
-        document.getElementById("oldskool6").style.zIndex = 0;
         document.getElementById("missCamille").style.zIndex = 1;
-        document.getElementById("missCamille1").style.zIndex = 1;
-        document.getElementById("missCamille2").style.zIndex = 1;
-        document.getElementById("missCamille3").style.zIndex = 1;
-        document.getElementById("missCamille4").style.zIndex = 1;
-        document.getElementById("missCamille5").style.zIndex = 1;
-        document.getElementById("missCamille6").style.zIndex = 1;
         document.querySelector(".switch-right").style.display = "none";
         document.querySelector(".switch-left").style.display = "block";
 
@@ -85,19 +67,7 @@ const Carousel = () => {
     switch (count) {
       case 1:
         document.getElementById("missCamille").style.zIndex = 0;
-        document.getElementById("missCamille1").style.zIndex = 0;
-        document.getElementById("missCamille2").style.zIndex = 0;
-        document.getElementById("missCamille3").style.zIndex = 0;
-        document.getElementById("missCamille4").style.zIndex = 0;
-        document.getElementById("missCamille5").style.zIndex = 0;
-        document.getElementById("missCamille6").style.zIndex = 0;
         document.getElementById("oldskool").style.zIndex = 0;
-        document.getElementById("oldskool1").style.zIndex = 0;
-        document.getElementById("oldskool2").style.zIndex = 0;
-        document.getElementById("oldskool3").style.zIndex = 0;
-        document.getElementById("oldskool4").style.zIndex = 0;
-        document.getElementById("oldskool5").style.zIndex = 0;
-        document.getElementById("oldskool6").style.zIndex = 0;
         document.getElementById("mscWeb").style.zIndex = 1;
         document.querySelector(".switch-left").style.display = "none";
         document.querySelector(".switch-right").style.display = "block";
@@ -120,8 +90,11 @@ const Carousel = () => {
   };
 
   return (
-    <section className="msc-carousel" id="msc">
+    <section className="msc-carousel" id="mscCarousel">
       <NavbarMsc />
+      <div className="title-msc">
+        <h1>Museum Sheet Cheat</h1>
+      </div>
       <div className="carousel-container">
         {/* pour le carousel grâce à une animation css en hover faire passer différentes photos du projet MSC et des autres projets selon le projets */}
         <button className="switch-left" onClick={handleClickCarouselLeft}>
@@ -130,30 +103,30 @@ const Carousel = () => {
         <div className="carousel">
           <div className="img-1">
             <img id="mscWeb" src={mscWeb} alt="" />
-            <img id="mscWeb1" src={mscWeb1} alt="" />
+            {/* <img id="mscWeb1" src={mscWeb1} alt="" />
             <img id="mscWeb2" src={mscWeb2} alt="" />
             <img id="mscWeb3" src={mscWeb3} alt="" />
             <img id="mscWeb4" src={mscWeb4} alt="" />
             <img id="mscWeb5" src={mscWeb5} alt="" />
-            <img id="mscWeb6" src={mscWeb6} alt="" />
+            <img id="mscWeb6" src={mscWeb6} alt="" /> */}
           </div>
           <div className="img-2">
             <img id="oldskool" src={oldskool} alt="" />
-            <img id="oldskool1" src={oldskool1} alt="" />
+           {/*  <img id="oldskool1" src={oldskool1} alt="" />
             <img id="oldskool2" src={oldskool2} alt="" />
             <img id="oldskool3" src={oldskool3} alt="" />
             <img id="oldskool4" src={oldskool4} alt="" />
             <img id="oldskool5" src={oldskool5} alt="" />
-            <img id="oldskool6" src={oldskool6} alt="" />
+            <img id="oldskool6" src={oldskool6} alt="" /> */}
           </div>
           <div className="img-3">
             <img id="missCamille" src={missCamille} alt="" />
-            <img id="missCamille1" src={missCamille1} alt="" />
+            {/* <img id="missCamille1" src={missCamille1} alt="" />
             <img id="missCamille2" src={missCamille2} alt="" />
             <img id="missCamille3" src={missCamille3} alt="" />
             <img id="missCamille4" src={missCamille4} alt="" />
             <img id="missCamille5" src={missCamille5} alt="" />
-            <img id="missCamille6" src={missCamille6} alt="" />
+            <img id="missCamille6" src={missCamille6} alt="" /> */}
           </div>
         </div>
         <button className="switch-right" onClick={handleClickCarouselRight}>
